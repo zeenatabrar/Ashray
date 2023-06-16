e91a7e9ab8f2ebf36ac94aac3d4d92c11087945c
Zeenat Abrar <zeenatabrar50@gmail.com>
2023-06-16 23:30:02 +0530

    add

--- main/index.js
+++ main/index.js
@@ -1,71 +1,73 @@
-const baseServerURL="https://hotels-tn92.onrender.com/hotels"
-let mainsection=document.getElementById("hotels")
 
-fetchdata(baseServerURL)
-function fetchdata(z){
-    fetch(z)
-    .then(function(res){
-        return res.json()
+document.addEventListener("DOMContentLoaded", () => {
+  const productContainers =
+    document.querySelectorAll(".product-container");
+  const nxtBtns = document.querySelectorAll(".nxt-btn");
+  const preBtns = document.querySelectorAll(".pre-btn");
+
+  productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtns[i].addEventListener("click", () => {
+      item.scrollLeft += containerWidth;
+    });
+
+    preBtns[i].addEventListener("click", () => {
+      item.scrollLeft -= containerWidth;
+    });
+  });
+
+  fetch("https://hotels-tn92.onrender.com/hotels?_sort=price&_order=asc")
+    .then((response) => response.json())
+    .then((data) => {
+      const productContainer =
+        document.getElementById("product-container");
+
+      // Create slider items
+      data.forEach((hotel) => {
+        var card = document.createElement("div");
+        card.className = "card";
+
+        var image = document.createElement("img");
+        image.src = hotel.image;
+        image.alt = "Hotel Image";
+        card.appendChild(image);
+
+        var details = document.createElement("div");
+        details.className = "card-details";
+        card.appendChild(details);
+
+        var h2 = document.createElement("h2");
+        h2.textContent = hotel.name;
+        details.appendChild(h2);
+
+        var p1 = document.createElement("p");
+        p1.textContent = hotel.location;
+        details.appendChild(p1);
+
+        // var p2 = document.createElement("p");
+        // p2.textContent = hotel.description;
+        // details.appendChild(p2);
+
+        var p3 = document.createElement("p");
+        p3.className = "price";
+        p3.textContent = "Price: ₹ " + hotel.price*10;
+        details.appendChild(p3);
+
+        var p4 = document.createElement("p");
+        p4.className = "rating";
+        p4.textContent = "Rating: " + hotel.rating;
+        details.appendChild(p4);
+
+        productContainer.appendChild(card);
+      });
     })
-    .then(function(data){
-        console.log(data)
-        display(data)
-    })
-    .catch(function(err){
-        console.log(err)
-    })
-}
+    .catch((error) => {
+      console.error("Error fetching hotel data:", error);
+    });
+});
 
-function display(data){
-    mainsection.innerHTML=""
-    let cardlist=document.createElement("div")
-    cardlist.className="cardlist"
-
-    mainsection.append(cardlist)
-
-    data.forEach((item)=>{
-        let card=createcard(item)
-        cardlist.append(card)
-    })
-}
-function createcard(item){
-    let card=document.createElement("div")
-    card.className="card"
-    card.setAttribute("data-id",item.id)
-
-    let imgdiv=document.createElement("div")
-    imgdiv.className = "card-img"
-
-  let img = document.createElement("img")
-  img.setAttribute("src", item.image)
-  img.setAttribute("alt", "hotels")
-
-  imgdiv.append(img)
-
-  let cardbody = document.createElement("div")
-  cardbody.className = "card-body"
-
-//   let h4 = document.createElement("h4")
-//   h4.className = "card-title"
-//   h4.innerText = item.description;
-
-  let p2 = document.createElement("p")
-  p2.className = "card-category"
-  p2.innerText = item.location
-
-  let p3 = document.createElement("p")
-  p3.className = "card-price"
-  p3.innerText = item.price;
-
-  
-  let p4 = document.createElement("p")
-  p4.className = "card-price"
-  p4.innerText = item.rating;
-
-  cardbody.append(p2,p3,p4)
-  card.append(imgdiv,cardbody)
-  return card;
-}
 
 
 // login & logout
